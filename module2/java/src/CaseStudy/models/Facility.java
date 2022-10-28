54bff0456e8c6f741f84cf890ae97d6fa7330c5c
unknown <you@example.com>
2022-10-28 12:57:45 +0700

    up

---
+++ module2/java/src/CaseStudy/models/Facility.java
@@ -0,0 +1,7 @@
+package CaseStudy.models;
+
+public abstract class Facility {
+    private String name;
+    private double price;
+
+}
--- module2/java/src/com/company/Main.java
+++ module2/java/src/com/company/Main.java
@@ -1,9 +1,33 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Main {
     public static void main(String[] args) {
-           SinhVien sinhVien = new SinhVien("A",12,"001");
-        System.out.println(sinhVien);
+//           SinhVien sinhVien = new SinhVien("A",12,"001");
+////        System.out.println(sinhVien);
+        ArrayList<String> car = new ArrayList<String>();
+        car.add("hi");
+        car.add("híhi");
+        car.add("a");
+        car.add("ab");
+        Collections.sort(car);
+        for (int i=0;i<car.size();i++){
+            System.out.println(car.get(i));
+       }
+//        ArrayList<Integer> numbers = new ArrayList<Integer>();
+//        numbers.add(10);
+//        numbers.add(9);
+//        numbers.add(13);
+//        numbers.add(12);
+//        numbers.add(56);
+//        Collections.sort(numbers);
+//        for (int i=0;i<numbers.size();i++){
+//            System.out.println(numbers.get(i));
+//        }
+
 
     }
 }
