5fa5aa6894729ceaed40cae4dea635e545dd4c99
unknown <you@example.com>
2022-12-31 17:18:10 +0700

    up

--- module3/case_study/20_task.sql
+++ module3/case_study/20_task.sql
@@ -112,6 +112,19 @@ where so_luong_dich_vu_di_kem = ( select max(so_luong_dich_vu_di_kem) from count
 -- Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung
 --  (được tính dựa trên việc count các ma_dich_vu_di_kem).
 
+-- 15.Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi 
+-- mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi, count(hd.ma_nhan_vien) tong_so_hop_dong from nhan_vien
+    join trinh_do using (ma_trinh_do)
+    join bo_phan using (ma_bo_phan)
+    join hop_dong hd using (ma_nhan_vien)
+where year(ngay_lam_hop_dong) between 2020 and 2021
+group by hd.ma_nhan_vien
+having tong_so_hop_dong <= 3
+order by ma_nhan_vien;
+
+
+
 
     
 
