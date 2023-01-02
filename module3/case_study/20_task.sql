148fd1a5c1561385a8b87d280ad332995f3a71f1
unknown <you@example.com>
2023-01-02 16:30:06 +0700

    up

--- module3/case_study/20_task.sql
+++ module3/case_study/20_task.sql
@@ -19,6 +19,13 @@ order by so_lan_dat_phong;
 -- (Với tổng tiền được tính theo công thức như sau: 
 -- Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet)
 --  cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+select ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc,
+if (hdct.ma_hop_dong is null, chi_phi_thue, chi_phi_thue + sum(so_luong*gia)) as 'Tổng tiền' from khach_hang kh
+inner join loai_khach using(ma_loai_khach) 
+left join hop_dong using(ma_khach_hang) left join hop_dong_chi_tiet hdct using(ma_hop_dong) left join dich_vu_di_kem using(ma_dich_vu_di_kem) 
+left join dich_vu using(ma_dich_vu)
+group by ma_khach_hang, ma_hop_dong
+order by ma_khach_hang;
 
 -- 6.Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
 -- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
