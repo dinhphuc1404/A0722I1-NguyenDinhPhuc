ef78c5cb35a5ac0842df35c0beb714a42ad9e9ce
unknown <you@example.com>
2023-05-17 20:40:35 +0700

    up

--- module5/ss3_angular_overview/bai_tap/ung-dung-may-tinh/src/app/app.module.ts
+++ module5/ss3_angular_overview/bai_tap/ung-dung-may-tinh/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CaculatorComponent } from './component/caculator/caculator.component';
+import { ColorComponent } from './component/color/color.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CaculatorComponent
+    CaculatorComponent,
+    ColorComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- module5/ss3_angular_overview/bai_tap/ung-dung-may-tinh/src/app/component/caculator/caculator.component.ts
+++ module5/ss3_angular_overview/bai_tap/ung-dung-may-tinh/src/app/component/caculator/caculator.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./caculator.component.css']
 })
 export class CaculatorComponent implements OnInit {
+  result: number = undefined;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  cong(firt: string, second: string) {
+    this.result = +firt + +second;
+  }
+
+  tru(firt: string, second: string) {
+    this.result = +firt - +second;
+  }
+
+  nhan(firt: string, second: string) {
+    this.result = +firt * +second;
+  }
+
+  chia(firt: string, second: string) {
+    this.result = +firt / +second;
+  }
 }
